6f90490e6c5950d5e860e55fa0a097b196fc66c9
SedativeHypnotics <sihantawsik@gmail.com>
2020-02-19 18:08:53 +0600

    used mysql

--- amar_recorder/settings.py
+++ amar_recorder/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "amar_project.apps.AmarProjectConfig",
 ]
 
 MIDDLEWARE = [
@@ -75,9 +76,11 @@ WSGI_APPLICATION = "amar_recorder.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/my.cnf',
+        },
     }
 }
 
